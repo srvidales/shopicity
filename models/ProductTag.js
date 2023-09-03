f2dd99cfb4c3defd54774b3059749940c6b08abe
Sergio Vidales Perez <srvidales@gmail.com>
2023-09-03 15:02:31 -0700

    Reformat all .js files

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,20 +1,21 @@
-const { Model, DataTypes } = require('sequelize');
+const {Model, DataTypes} = require('sequelize');
 
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model {
+}
 
 ProductTag.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
-  }
+    {
+        // define columns
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'product_tag',
+    }
 );
 
 module.exports = ProductTag;
--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+    console.log(`App listening on port ${PORT}!`);
 });
