2978ce5501309ad79958f85e141872337c2da6ec
Sergio Vidales Perez <srvidales@gmail.com>
2023-09-17 04:53:15 -0700

    Add Tag post, put and delete

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -25,14 +25,42 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
     // create a new tag
+    Tag.create(req.body)
+        .then((newTag) => {
+            res.json(newTag);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        })
 });
 
 router.put('/:id', (req, res) => {
     // update a tag's name by its `id` value
+    Tag.update({
+            tag_name: req.body.tag_name
+        },
+        {
+            where: {
+                id: req.params.id,
+            },
+        })
+        .then((updatedTag) => {
+            res.json(updatedTag);
+        })
+        .catch((err) => res.json(err));
 });
 
 router.delete('/:id', (req, res) => {
     // delete on tag by its `id` value
+    Tag.destroy({
+        where: {
+            id: req.params.id,
+        },
+    })
+        .then((deletedTag) => {
+            res.json(deletedTag);
+        })
+        .catch((err) => res.json(err));
 });
 
 module.exports = router;
