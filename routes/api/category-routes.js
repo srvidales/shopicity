c47bbca46ad5cefbd13a5826fc0ee2c34d1a42fd
Sergio Vidales Perez <srvidales@gmail.com>
2023-09-17 04:46:00 -0700

    Add Category post, put and delete

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -25,14 +25,42 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
     // create a new category
+    Category.create(req.body)
+        .then((newCategory) => {
+            res.json(newCategory);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        })
 });
 
 router.put('/:id', (req, res) => {
     // update a category by its `id` value
+    Category.update({
+            category_name: req.body.category_name
+        },
+        {
+            where: {
+                id: req.params.id,
+            },
+        })
+        .then((updatedCategory) => {
+            res.json(updatedCategory);
+        })
+        .catch((err) => res.json(err));
 });
 
 router.delete('/:id', (req, res) => {
     // delete a category by its `id` value
+    Category.destroy({
+        where: {
+            id: req.params.id,
+        },
+    })
+        .then((deletedCategory) => {
+            res.json(deletedCategory);
+        })
+        .catch((err) => res.json(err));
 });
 
 module.exports = router;
